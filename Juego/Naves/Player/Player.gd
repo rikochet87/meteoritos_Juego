a8eff3d02cb62925d8b5ab2185902b0ed4d24bf0
Matias <matias3282@hotmail.com>
2022-10-11 19:41:56 -0300

    Motor
    Progresión del sonido del motor

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -5,13 +5,15 @@ extends RigidBody2D
 ## Atributos export
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 280
+export var estela_maxima:int = 150
 ## Atributos
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
 ## Atributos onready
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
-
+onready var estela:Estela = $EstelaPuntoInicio/Trail2D
+onready var motor_sfx:Motor = $MotorSFX
 ## Metodos
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -20,7 +22,16 @@ func _unhandled_input(event: InputEvent) -> void:
 		laser.set_is_casting(true)
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)	
-
+	# Control estela y sonido motor
+	if event.is_action_pressed("mover_adelante"):
+		estela.set_max_points(estela_maxima)
+		motor_sfx.sonido_on()
+	elif event.is_action_pressed("mover_atras"):
+		estela.set_max_points(0)
+		motor_sfx.sonido_on()	
+	if(event.is_action_released("mover_adelante")or event.is_action_released("mover_atras")):
+		motor_sfx.sonido_off()	
+		
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
@@ -49,3 +60,5 @@ func player_input() -> void:
 		canion.set_esta_disparando(true)
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)			
+
+	
