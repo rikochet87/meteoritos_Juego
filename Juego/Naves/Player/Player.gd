aaccedc2d92a298496868046cf411fdda0a34981
Matias <matias3282@hotmail.com>
2022-10-13 19:21:52 -0300

    Maquina de estados finitos, spawnear

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -2,21 +2,33 @@
 class_name Player
 extends RigidBody2D
 
+## Enums
+enum ESTADO {SPAWN, VIVO, INVENCIBLE, MUERTO}
+
 ## Atributos export
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 280
 export var estela_maxima:int = 150
+
 ## Atributos
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
+var estado_actual:int = ESTADO.SPAWN
+
 ## Atributos onready
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
 onready var estela:Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx:Motor = $MotorSFX
-## Metodos
+onready var colisionador:CollisionShape2D = $CollisionShape2D
 
+## Metodos
+func _ready() -> void:
+	controlador_estados(estado_actual)
+	
 func _unhandled_input(event: InputEvent) -> void:
+	if not esta_input_activo():
+		return
 	# Disparo rayo
 	if event.is_action_pressed("disparo_secundario"):
 		laser.set_is_casting(true)
@@ -40,7 +52,34 @@ func _process(delta: float) -> void:
 	player_input()
 	
 ## Metodos Custom	
+func controlador_estados(nuevo_estado: int) -> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disabled", false)
+			canion.set_puede_disparar(true)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled", true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(true)
+			queue_free()
+		_:
+			printerr("Error de estado")
+	
+	estado_actual = nuevo_estado							
+
+func esta_input_activo() ->bool:
+	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
+		return false
+		
+	return true	
+
 func player_input() -> void:
+	if not esta_input_activo():
+		return
 	# Empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("mover_adelante"):
@@ -53,7 +92,7 @@ func player_input() -> void:
 	if Input.is_action_pressed("rotar_antihorario"):
 		dir_rotacion -= 1
 	elif Input.is_action_pressed("rotar_horario"):
-		dir_rotacion += 1
+		dir_rotacion += 1		
 			
 	## Disparo
 	if Input.is_action_pressed("disparo_principal"):
@@ -61,4 +100,7 @@ func player_input() -> void:
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)			
 
-	
+## Señales Internas	
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "spawn":
+		controlador_estados(ESTADO.VIVO)
