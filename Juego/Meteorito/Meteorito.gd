29992836bf2dcb6c5f4f868942b869284ce0836e
Matias <matias3282@hotmail.com>
2022-10-26 18:03:29 -0300

    Zona de peligro
    -Lluvia de Meteoritos dinámica
    -Atributos del sector
    -Cámaras

--- Juego/Meteorito/Meteorito.gd
+++ Juego/Meteorito/Meteorito.gd
@@ -13,6 +13,24 @@ onready var animacion_impacto:AnimationPlayer = $AnimationPlayer
 
 ## Atributos
 var hitpoints:float
+var esta_en_sector:bool = true setget set_esta_en_sector
+var pos_spawn_original:Vector2
+var vel_spawn_original:Vector2
+
+## Setters y Getters
+func set_esta_en_sector(valor: bool) -> void:
+	esta_en_sector = valor
+
+## Metodos
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	if esta_en_sector:
+		return
+		
+	var mi_transform := state.get_transform()
+	mi_transform.origin = pos_spawn_original
+	linear_velocity = vel_spawn_original
+	state.set_transform(mi_transform)
+	esta_en_sector = true	
 
 ## Metodos Custom
 func recibir_danio(danio: float) ->void:
@@ -36,6 +54,7 @@ func aleatorizar_velocidad() -> float:
 ## constructor
 func crear(pos:Vector2, dir:Vector2, tamanio:float)->void:
 	position = pos
+	pos_spawn_original = position
 	mass *= tamanio
 	$Sprite.scale = Vector2.ONE * tamanio
 	var radio:int = int($Sprite.texture.get_size().x / 2.3 * tamanio)
@@ -43,6 +62,7 @@ func crear(pos:Vector2, dir:Vector2, tamanio:float)->void:
 	forma_colision.radius = radio
 	$CollisionShape2D.shape = forma_colision
 	linear_velocity = (vel_lineal_base * dir / tamanio) * aleatorizar_velocidad() 
+	vel_spawn_original = linear_velocity
 	angular_velocity = (vel_ang_base / tamanio) * aleatorizar_velocidad()
 	hitpoints = hitpoints_base * tamanio
 	
