7255d0e6c9fbdd9ac0bbb7108094b1bd058fb126
Matias <matias3282@hotmail.com>
2022-10-31 18:40:09 -0300

    Estacion Recarga
    recarga de escudo y laser

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -18,12 +18,12 @@ var estado_actual:int = ESTADO.SPAWN
 
 ## Atributos onready
 onready var canion:Canion = $Canion
-onready var laser:RayoLaser = $LaserBeam2D
+onready var laser:RayoLaser = $LaserBeam2D setget ,get_laser
 onready var estela:Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx:Motor = $MotorSFX
 onready var colisionador:CollisionShape2D = $CollisionShape2D
 onready var impacto_sfx:AudioStreamPlayer = $ImpactosSFX
-onready var escudo:Escudo = $Escudo
+onready var escudo:Escudo = $Escudo setget ,get_escudo
 
 ## Metodos
 func _ready() -> void:
@@ -125,3 +125,10 @@ func _on_body_entered(body: Node) -> void:
 	if body is Meteorito:
 		body.destruir()
 		destruir()
+
+## Setter y Getter
+func get_laser() -> RayoLaser:
+	return laser
+	
+func get_escudo() -> Escudo:
+	return escudo	
