93ebb8924a06a451d208791800b4001af01f8737
Matias <matias3282@hotmail.com>
2022-11-01 17:36:38 -0300

    Pulido
    -Meteoritos
    -Camara de destruccion player
    -Explosiones Player

--- Juego/Atoloads/Eventos.gd
+++ Juego/Atoloads/Eventos.gd
@@ -1,7 +1,8 @@
 extends Node
 
 signal disparo(proyectil)
-signal nave_destruida(posicion, explosiones)
+signal nave_destruida(nave, posicion, explosiones)
 signal spawn_meteorito(posicion, direccion, tamanio)
 signal nave_en_sector_peligro(centro_camara, tipo_peligro, numero_peligros)
 signal meteorito_destruido(posicion)
+
